cbbebcccc1417dd2e9bd9f0768f915849ff37942
Masterjoona <69722179+Masterjoona@users.noreply.github.com>
2024-07-29 17:53:42 +0300

    init

---
+++ README.md
@@ -0,0 +1,3 @@
+## vc-inboxmyfavorites
+
+Shows messages from channels that are favorited but are muted at server/category level in the inbox.
---
+++ index.ts
@@ -0,0 +1,23 @@
+/*
+ * Vencord, a Discord client mod
+ * Copyright (c) 2024 Vendicated and contributors
+ * SPDX-License-Identifier: GPL-3.0-or-later
+ */
+
+import { Devs } from "@utils/constants";
+import definePlugin from "@utils/types";
+
+export default definePlugin({
+    name: "InboxMyFavorites",
+    authors: [Devs.Joona],
+    description: "Inbox favorited channelsw notifications from servers/categories that are muted",
+    patches: [
+        {
+            find: "this channel should already be loaded",
+            replacement: {
+                match: /lMuted\(\i,(\i\.id)\)(?=.+?(\i\.\i\.isFavorite))/,
+                replace: "$& && !$2($1)"
+            }
+        }
+    ],
+});
